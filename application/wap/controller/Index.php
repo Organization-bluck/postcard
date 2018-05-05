6c7f47dd166f0d5cd75943188d251b0241538e80
许许恒 <1197256140@qq.com>
2018-05-06 00:05:44 +0800

    录音上传七牛云

--- application/wap/controller/Index.php
+++ application/wap/controller/Index.php
@@ -2,10 +2,7 @@
 
 namespace app\wap\controller;
 
-use app\manager\controller\Vip;
-use service\DataService;
-use service\ExtendService;
-use service\PayService;
+use service\FileService;
 use think\Db;
 use think\Exception;
 
@@ -20,7 +17,24 @@ class Index extends Base
 
     public function uploadRecode()
     {
-        var_dump(input('post.'));
-        exit;
+        try{
+            $serviceId = input('post.serverId/s');
+            if(!$serviceId) {
+                throw new Exception('信息不存在');
+            }
+            $source_info = load_wechat('media')->getMedia($serviceId);
+            $file_info = FileService::qiniu($this->fansinfo['nickname'].uniqid().'.amr', $source_info);
+            if(isset($file_info['url'])) {
+                if(!Db::table('post_media')->insert([
+                    'openid'    => $this->openid,
+                    'file_url'  => $file_info['url']
+                ])) {
+                    throw new Exception('录音添加失败');
+                }
+            }
+            $this->success('操作成功', '', $file_info['url']);
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
     }
 }
